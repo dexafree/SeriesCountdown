74699274b8a53a60d335b3c30b46e9a18778bd39
Dexafree <dexafree90@gmail.com>
2015-11-15 12:01:52 +0100

    Native map and filter functions

--- app/src/main/java/com/dexafree/seriescountdown/utils/Utils.java
+++ app/src/main/java/com/dexafree/seriescountdown/utils/Utils.java
@@ -2,10 +2,39 @@ package com.dexafree.seriescountdown.utils;
 
 import android.os.Build;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import rx.functions.Func1;
+
 public class Utils {
 
     public static boolean isLollipopOrHigher(){
         return Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP;
     }
+
+    public static <T, K> List<K> map(Iterable<T> list, Func1<T, K> function){
+        List<K> newList = new ArrayList<>();
+
+        for(T item : list){
+            newList.add(function.call(item));
+        }
+
+        return newList;
+    }
+
+    public static <T> List<T> filter(Iterable<T> list, Func1<T, Boolean> function){
+        List<T> newList = new ArrayList<>();
+
+        for(T item : list){
+            if(function.call(item)) {
+                newList.add(item);
+            }
+        }
+
+        return newList;
+    }
+
+
 
 }
